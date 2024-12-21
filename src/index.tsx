9c4d3e479f0c9b8181f9bf8498fca4693886d8b8
broo <zarifullin17022005@icloud.com>
2024-12-21 21:17:32 +0300

    change config

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import "./shared/config/i18n/i18n";
 import ErrorBoundary from "app/providers/ErrorBoundary/ErrorBoundary";
 
 
+console.log(1)
+
 const root = createRoot(document.getElementById("root"));
 root.render(
     <BrowserRouter>
