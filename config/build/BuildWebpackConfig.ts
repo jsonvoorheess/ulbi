9e5a104f78ee3cdc7211fda5c62714d8f7e3edf4
broo <zarifullin17022005@icloud.com>
2024-12-21 23:18:30 +0300

    change yml file

--- config/build/BuildWebpackConfig.ts
+++ config/build/BuildWebpackConfig.ts
@@ -16,11 +16,6 @@ export function BuildWebpackConfig(options: BuildOptions):webpack.Configuration 
             clean: true
         },
         devServer: buildDevServer(options),
-        performance: {
-            hints: false,
-            maxEntrypointSize: 512000,
-            maxAssetSize: 512000
-        },
         plugins: buildPlugins(options),
         module: {
             rules: buildLoaders(options),
